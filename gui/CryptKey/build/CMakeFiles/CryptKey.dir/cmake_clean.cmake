f2ff985b551b6be9d6935030d78f8143e6a44bd9
So-ny <syonchau@gmail.com>
2025-04-07 23:34:14 -0700

    Some changes

---
+++ gui/CryptKey/build/CMakeFiles/CryptKey.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CryptKey_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/CryptKey_autogen.dir/ParseCache.txt"
+  "CryptKey_autogen"
+  "CMakeFiles/CryptKey.dir/CryptKey_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/CryptKey.dir/CryptKey_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/CryptKey.dir/main.cpp.o"
+  "CMakeFiles/CryptKey.dir/main.cpp.o.d"
+  "CMakeFiles/CryptKey.dir/mainwindow.cpp.o"
+  "CMakeFiles/CryptKey.dir/mainwindow.cpp.o.d"
+  "CryptKey.app/Contents/MacOS/CryptKey"
+  "CryptKey.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CryptKey.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
